efdd4c996eb127c6c3a06ccf175bfadaf6941f78
Francesco Bettella <checco74@gmail.com>
2020-07-15 17:38:58 +0200

    streamline + add options

--- gitexport.sh
+++ gitexport.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/env bash
+# exports a repository to a gzip-compressed tarball
 
 # exit on error
 set -ETeuo pipefail
 
-[ -d ".git" ] || { printf "error at %d\n" $LINENO >&2; exit 1; }
+[ -d ".git" ] || { printf "error at %d.\n" $LINENO >&2; exit 1; }
 
-[ "$*" != "" ] || { printf "usage: %s <dir>\n" $0; exit 0; }
+[ "$*" != "" ] || { printf "usage: %s <dir>\n" ${BASH_SOURCE[0]}; exit 0; }
 
 EXPDIR="$1"
 
 LOCTMPDIR=/tmp
+[ -z "${TMPDIR}" ] || LOCTMPDIR="${TMPDIR}"
 
-PRJNAME='genimpute'
-PRJDIR="$( cd $( dirname $( readlink -f "$0" ) )/../ ; pwd )"
+PRJDIR="$( cd $( dirname $( readlink -e "${BASH_SOURCE[0]}" ) )/../ ; pwd )"
+PRJDIR=$( readlink -e $PRJDIR )
+PRJNAME=$( basename $PRJDIR )
 
 GITROOT="$(git rev-parse --show-toplevel)"
 # calculate md5, but only use first 6 chars
 MD5=$(find ${GITROOT}/.git -type f | xargs cat | md5sum)
 MD5=${MD5:0:7}
 
+echo "Cloning source repository '${PRJDIR}'..."
 cd ${LOCTMPDIR} && rm -rf ${PRJNAME}
 git clone ${PRJDIR} ${PRJNAME}
+echo "done."
 
 echo "Compressing into '${PRJNAME}.git.tar.gz'..."
 tar czf ${PRJNAME}.git.tar.gz ${PRJNAME}
--- pushgithub.sh
+++ pushgithub.sh
@@ -4,21 +4,43 @@
 set -ETeuo pipefail
 
 declare -r MAXFILESIZE=50 #MB
-declare -r REMOTEREPOS='origin_gh_norm'
+declare -r BRANCH_DEF='master'
+declare BRANCH=$BRANCH_DEF
+declare -r PUBREPNAME_DEF='origin'
+declare PUBREPNAME=$PUBREPNAME_DEF
 
-if [ "$*" == "" ] ; then
-  echo "no public repository specified."
-  echo "usage: $( basename $0 ) <path_to_repository>"
-  echo " e.g.: $( basename $0 ) https://github.com/scimerc/genimpute.git"
+while getopts "b:n:" opt; do
+case "${opt}" in
+  n)
+    BRANCH="${OPTARG}"
+    echo "branch set to: ${BRANCH}"
+    ;;
+  n)
+    PUBREPNAME="${OPTARG}"
+    echo "remote repository name set to: ${PUBREPNAME}"
+    ;;
+esac
+done
+shift $((OPTIND-1))
+
+if [ "$1" == "" ] ; then
+  cat << EOF
+no public repository specified.
+usage: $( basename ${BASH_SOURCE[0]} ) \\
+[-b <branch='$BRANCH_DEF'>] \\
+[-n <name='$PUBREPNAME_DEF'>] \\
+<path_to_repository>"
+ e.g.: $( basename ${BASH_SOURCE[0]} ) https://github.com/scimerc/myrepo.git"
+EOF
   exit 0
 fi
 
 PUBREP="$1"
-PRJDIR="$( cd $( dirname $( readlink -f "$0" ) )/../ ; pwd )"
+PRJDIR="$( cd $( dirname $( readlink -e "${BASH_SOURCE[0]}" ) )/../ ; pwd )"
 
 cd ${PRJDIR}
 
-lflist=$( find . -type f -size +${MAXFILESIZE}M -not -path '*.git*'  )
+lflist=$( find . -type f -size +${MAXFILESIZE}M -not -path '*.git*' )
 
 if [ "${lflist}" != "" ] ; then
 
@@ -33,8 +55,9 @@ fi
 echo 'committing and pushing changes to remote..'
 
 git add .
-git remote -v | grep -qw "${REMOTEREPOS}" || git remote add ${REMOTEREPOS} ${PUBREP}
-git pull ${REMOTEREPOS} master
+git remote -v | grep -qw "${PUBREPNAME}" || git remote add ${PUBREPNAME} ${PUBREP}
+git pull ${PUBREPNAME} ${BRANCH}
 git commit -m 'merge with remote'
-git push ${REMOTEREPOS}
+git push -u ${PUBREPNAME}
+git push
 
