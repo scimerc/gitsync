10a5a4c564190163bfa39821bf4580de50716b22
Francesco Bettella <checco74@gmail.com>
2020-07-16 13:33:04 +0200

    actually sync things other than self

--- gitexport.sh
+++ gitexport.sh
@@ -4,35 +4,54 @@
 # exit on error
 set -ETeuo pipefail
 
-[ -d ".git" ] || { printf "error at %d.\n" $LINENO >&2; exit 1; }
+[ "$*" != "" ] || { printf "Usage: %s <dir(s)>\n" ${BASH_SOURCE[0]}; exit 0; }
 
-[ "$*" != "" ] || { printf "usage: %s <dir>\n" ${BASH_SOURCE[0]}; exit 0; }
+CWDIR=$(pwd)
 
-EXPDIR="$1"
+LOCTMPDIR="${TMPDIR:-/tmp}"
 
-LOCTMPDIR=/tmp
-[ -z "${TMPDIR}" ] || LOCTMPDIR="${TMPDIR}"
+PRJDIRS=( "$@" )
 
-PRJDIR="$( cd $( dirname $( readlink -e "${BASH_SOURCE[0]}" ) )/../ ; pwd )"
-PRJDIR=$( readlink -e $PRJDIR )
-PRJNAME=$( basename $PRJDIR )
+for k in ${!PRJDIRS[@]} ; do
 
-GITROOT="$(git rev-parse --show-toplevel)"
-# calculate md5, but only use first 6 chars
-MD5=$(find ${GITROOT}/.git -type f | xargs cat | md5sum)
-MD5=${MD5:0:7}
+  if [ ! -d "${PRJDIRS[$k]}/.git" ] ; then
+    printf "No git repository in '%s': skipping..\n" "${PRJDIRS[$k]}"
+    continue
+  fi
 
-echo "Cloning source repository '${PRJDIR}'..."
-cd ${LOCTMPDIR} && rm -rf ${PRJNAME}
-git clone ${PRJDIR} ${PRJNAME}
-echo "done."
+  PRJDIRS[$k]=$( readlink -e ${PRJDIRS[$k]} )
+  PRJNAME=$( basename ${PRJDIRS[$k]} )
 
-echo "Compressing into '${PRJNAME}.git.tar.gz'..."
-tar czf ${PRJNAME}.git.tar.gz ${PRJNAME}
-echo "done."
+  # calculate md5, but only use first 6 chars
+  MD5=$(find ${PRJDIRS[$k]}/.git -type f | xargs cat | md5sum)
+  MD5=${MD5:0:7}
+  OUTFILENAME=${PRJNAME}_"$(date +"%y%m%d-%H")"_${MD5}.git.tar.gz
+  if [ -f "${OUTFILENAME}" ] ; then
+    echo -n "'${OUTFILENAME}' exists in the current directory ['${CWDIR}']: "
+    if [[ $- == *i* ]] ; then
+      read -p "Do you want to overwrite it?[Y/n]:" -n 1 -r
+      echo
+      if [[ ! $REPLY =~ ^([Yy]|)$ ]]; then
+        return 0
+      fi
+    else
+      echo -e "\nNon-interactive parent shell detected: skipping.."
+      continue
+    fi
+  fi
 
-declare -r OUTFILENAME=${PRJNAME}_"$(date +"%y%m%d-%H")"_${MD5}.git.tar.gz
-mv ${PRJNAME}.git.tar.gz "${EXPDIR}/${OUTFILENAME}"
+  echo "Cloning source repository '${PRJDIRS[$k]}'..."
+  cd ${LOCTMPDIR} && rm -rf "${PRJNAME}"
+  git clone ${PRJDIRS[$k]} ${PRJNAME}
 
-echo "Repository exported to '${EXPDIR}/${OUTFILENAME}'."
+  echo "Compressing into '${PRJNAME}.git.tar.gz'..."
+  tar czf ${PRJNAME}.git.tar.gz ${PRJNAME}
+
+  mv ${PRJNAME}.git.tar.gz "${CWDIR}/${OUTFILENAME}"
+
+  echo "Repository exported to '${CWDIR}/${OUTFILENAME}'."
+
+  cd "${CWDIR}"
+
+done
 
--- pushgithub.sh
+++ pushgithub.sh
@@ -8,56 +8,95 @@ declare -r BRANCH_DEF='master'
 declare BRANCH=$BRANCH_DEF
 declare -r PUBREPNAME_DEF='origin'
 declare PUBREPNAME=$PUBREPNAME_DEF
+declare -r PUBREP_DEF='file://tmp'
+declare PUBREP=$PUBREP_DEF
 
 if [ "$*" == "" ] ; then
   cat << EOF
-no public repository specified.
-usage: $( basename ${BASH_SOURCE[0]} ) \
-[-b <branch='$BRANCH_DEF'>] \
-[-n <name='$PUBREPNAME_DEF'>] \
-<path_to_repository>
- e.g.: $( basename ${BASH_SOURCE[0]} ) https://github.com/scimerc/myrepo.git
+No repository specified.
+
+USAGE:  $( basename ${BASH_SOURCE[0]} ) [OPTIONS] <path_to_repository>
+
+OPTIONS:
+  -b <branch='$BRANCH_DEF'>
+  -n <name='$PUBREPNAME_DEF'>
+  -r <remote='$PUBREP_DEF'
+
+e.g.:
+$( basename ${BASH_SOURCE[0]} ) -r https://github.com/scimerc myrepo
+will push to the remote github https://github.com/scimerc/myrepo[.git]
 EOF
   exit 0
 fi
 
-while getopts "b:n:" opt; do
+while getopts "b:n:r:" opt; do
 case "${opt}" in
   n)
     BRANCH="${OPTARG}"
-    echo "branch set to: ${BRANCH}"
+    echo "Branch set to: ${BRANCH}"
     ;;
   n)
     PUBREPNAME="${OPTARG}"
-    echo "remote repository name set to: ${PUBREPNAME}"
+    echo "Remote repository name set to: ${PUBREPNAME}"
+    ;;
+  n)
+    PUBREP="${OPTARG}"
+    echo "Remote repository address set to: ${PUBREP}"
     ;;
 esac
 done
 shift $((OPTIND-1))
 
-PUBREP="$1"
-PRJDIR="$( cd $( dirname $( readlink -e "${BASH_SOURCE[0]}" ) ) ; pwd )"
+CWDIR=$(pwd)
 
-cd ${PRJDIR}
+for PRJDIR in "$@" ; do
 
-lflist=$( find . -type f -size +${MAXFILESIZE}M -not -path '*.git*' )
+  cd ${PRJDIR}
+  
+  echo -e "\n${PRJDIR}:"
 
-if [ "${lflist}" != "" ] ; then
+  if [ ! -d .git ] ; then
+    printf "No git repository in '%s': skipping..\n" "${PRJDIR}"
+    cd ${CWDIR}
+    continue
+  fi
 
-  echo 'adding large files support..'
+  if [ -n "$( git status --porcelain )" ] ; then
+    printf "Unclean repository in '%s': skipping..\n" "${PRJDIR}"
+    cd ${CWDIR}
+    continue
+  fi
 
-  git lfs track ${lflist}
-  git add .gitattributes ${lflist}
-  git commit -m 'lfs' || true
+  lflist=$( find . -type f -size +${MAXFILESIZE}M -not -path '*.git*' )
 
-fi
+  if [ "${lflist}" != "" ] ; then
 
-echo 'committing and pushing changes to remote..'
+    echo 'Adding large files support..'
 
-git add .
-git remote -v | grep -qw "${PUBREPNAME}" || git remote add ${PUBREPNAME} ${PUBREP}
-git pull ${PUBREPNAME} ${BRANCH}
-git commit -m 'merge with remote'
-git push -u ${PUBREPNAME}
-git push
+    git lfs track ${lflist}
+    git add .gitattributes ${lflist}
+    git commit -m 'lfs' || true
+
+  fi
+
+  echo 'Committing and pushing changes to remote..'
+
+  git add .
+  if git remote -v | grep -qw "${PUBREPNAME}" ; then
+    echo "Ignoring [${PUBREPNAME}]'${PUBREP}', as ${PRJDIR} already has a remote by that name."
+    git remote set-branches --add ${PUBREPNAME} ${BRANCH}
+  else
+    echo "Adding [${PUBREPNAME}]'${PUBREP}' remote to repository ${PRJDIR}."
+    git remote add -t ${BRANCH} ${PUBREPNAME} ${PUBREP}
+  fi
+  git pull ${PUBREPNAME} ${BRANCH}
+  git commit -m 'merge with remote'
+  git push -u ${PUBREPNAME}
+  git push
+  
+  cd ${CWDIR}
+
+  echo
+
+done
 
